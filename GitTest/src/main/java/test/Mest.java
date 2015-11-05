bf06bec100055f7bea27960c66434024384876bb
osvyryd <olastytsyuk@gmail.com>
2015-11-05 22:41:20 +0200

    Message

--- GitTest/src/main/java/test/Mest.java
+++ GitTest/src/main/java/test/Mest.java
@@ -4,6 +4,6 @@ public class Mest {
 	public static void main(String[] args) {
 		System.out.println("Some changes made by olga");
 		System.out.println("And from local");
-		System.out.println("And once more");
+		System.out.println("And once more.And more");
 	}
 }
