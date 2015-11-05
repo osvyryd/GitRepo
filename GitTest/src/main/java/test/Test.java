401fd0094813e534f309cab6f4b6186d0ee6b3f4
osvyryd <olastytsyuk@gmail.com>
2015-11-05 22:24:42 +0200

    Add changes to branch.

--- GitTest/src/main/java/test/Test.java
+++ GitTest/src/main/java/test/Test.java
@@ -2,6 +2,6 @@ package test;
 
 public class Test {
 	public static void main(String[] args) {
-		System.out.println("Hello. And happy to see you here, on Git! Pum-pum-purum");
+		System.out.println("Hello. And happy to see you here, on Git! Pum-pum-purum. This is from branch");
 	}
 }
