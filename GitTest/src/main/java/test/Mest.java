2cf92400806403c9b8a3139c44905f842b96a257
Olga Svyryd <osvyryd@atlassian.com>
2024-02-13 09:39:38 +0200

    Test1: remove test1

--- GitTest/src/main/java/test/Mest.java
+++ GitTest/src/main/java/test/Mest.java
@@ -4,6 +4,6 @@ public class Mest {
 	public static void main(String[] args) {
 		System.out.println("Some changes made by olga. Dev.");
 		System.out.println("And from local");
-		System.out.println("And once more.And more+test1");
+		System.out.println("And once more.And more");
 	}
 }
