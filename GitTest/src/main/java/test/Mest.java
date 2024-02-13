cff44a77bb806567dc5e1bf714d6cb6861fbfebf
Olga Svyryd <osvyryd@atlassian.com>
2024-02-13 09:48:15 +0200

    Master: master change

--- GitTest/src/main/java/test/Mest.java
+++ GitTest/src/main/java/test/Mest.java
@@ -3,7 +3,7 @@ package test;
 public class Mest {
 	public static void main(String[] args) {
 		System.out.println("Some changes made by olga. Dev.");
-		System.out.println("And from local");
+		System.out.println("And from local+master");
 		System.out.println("And once more.And more");
 	}
 }
